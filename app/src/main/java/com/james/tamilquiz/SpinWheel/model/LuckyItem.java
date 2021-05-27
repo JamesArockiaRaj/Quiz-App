34f8db45bc8a98cf64c3bdb6b1ce28235f14823f
JamesArockiaRaj <iamjamesarockiaraj>
2021-05-27 23:19:13 +0530

    Quiz-App-With-Earning-System

---
+++ app/src/main/java/com/james/tamilquiz/SpinWheel/model/LuckyItem.java
@@ -0,0 +1,11 @@
+package com.james.tamilquiz.SpinWheel.model;
+
+
+public class LuckyItem {
+    public String topText;
+    public String secondaryText;
+    public int secondaryTextOrientation;
+    public int icon;
+    public int color;
+    public int textColor;
+}
